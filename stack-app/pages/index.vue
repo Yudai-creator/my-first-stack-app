2df44f148c661f2f5128d4f2b02a67d483dcd01b
Yudai-creator <perezheber73@gmail.com>
2022-03-22 10:33:14 -0400

    nuxt app with clarinet setup

---
+++ stack-app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1 class="text-3xl font-bold text-orange-500">
+      My first stack app
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style>
+  body{
+    background-color: rgb(27, 27, 27);
+  }
+</style>
